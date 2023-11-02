8e05ac0e7a5234556acd83528cc5b6f3b56a769c
Stefan Scholz <mail@stefan-scholz.eu>
2023-11-02 23:57:43 +0100

    update session 4 5 and assignment 6

--- scripts/playwright_whatsmyuseragent_screenshot.py
+++ scripts/playwright_whatsmyuseragent_screenshot.py
@@ -4,7 +4,7 @@ with sync_playwright() as p:
     for browser_type in [p.chromium, p.firefox]:
         browser = browser_type.launch()
         page = browser.new_page()
-        page.goto('http://whatsmyuseragent.org/')
+        page.goto('https://www.whatismybrowser.com/')
         _ = page.screenshot(path=f'figures/example-{browser_type.name}.png')
         browser.close()
 
